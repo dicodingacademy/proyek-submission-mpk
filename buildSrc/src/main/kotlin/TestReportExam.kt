a6b9e4d0fa152b70b702d5fd176324a464f881f9
alfianyusufabdullah <alfianyabdullah@gmail.com>
2022-07-29 15:38:34 +0700

    generate result as a json file

--- buildSrc/src/main/kotlin/TestReportExam.kt
+++ buildSrc/src/main/kotlin/TestReportExam.kt
@@ -1,8 +1,11 @@
+import com.google.gson.GsonBuilder
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.w3c.dom.Element
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
+
+data class Result (val isPassed: Boolean, val testcaseName: String, val errorMessage: String?)
 
 abstract class TestReportExam : DefaultTask() {
     @TaskAction
@@ -20,6 +23,7 @@ abstract class TestReportExam : DefaultTask() {
 
         doc.normalizeDocument()
 
+        val results = mutableListOf<Result>()
         val testcaseNodes = doc.getElementsByTagName("testcase")
         repeat(testcaseNodes.length) {
             val testsuite = testcaseNodes.item(it) as Element
@@ -27,16 +31,20 @@ abstract class TestReportExam : DefaultTask() {
             if (failure !== null) {
                 val testcaseName = testsuite.getAttribute("name")
                 val errorMessage = failure.getAttribute("message").split("AssertionFailedError: ").last()
-
                 colored(enabled = true) {
-                    println( " ERROR ".red.bold + " " + errorMessage.bold.reverse + " " + testcaseName)
+                    println( " ERROR ".red.bold + " " + errorMessage.bold.reverse + " " + testcaseName + " " + testsuite.getAttribute("name"))
                 }
+                results.add(Result(false, testcaseName, errorMessage))
             } else {
                 colored(enabled = true) {
                     println(" SUCCESS ".green.bold + testsuite.getAttribute("name"))
                 }
+                results.add(Result(true, testsuite.getAttribute("name"), null))
             }
         }
+
+        val result = GsonBuilder().setPrettyPrinting().create()
+        File(project.projectDir.toString() +  "/result.json").writeText(result.toJson(results))
 
     }
 } 
