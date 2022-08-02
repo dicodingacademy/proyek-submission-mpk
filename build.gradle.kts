1015b119f5892b798153b2d218cb8bf08348991c
alfianyusufabdullah <alfianyabdullah@gmail.com>
2022-08-02 09:03:53 +0700

    bump library and kotlin ver

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.0"
     java
 }
 
@@ -17,12 +17,9 @@ tasks.withType<Test> {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-
-    testImplementation ("org.amshove.kluent:kluent:1.68")
-
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.17")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.17")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.18")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.18")
     testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
