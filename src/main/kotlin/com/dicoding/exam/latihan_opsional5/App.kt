80cccd6fe337b8030b99c523371a7223d16917d1
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-08-28 14:42:53 +0700

    feat: add `optional exam 5`

---
+++ src/main/kotlin/com/dicoding/exam/latihan_opsional5/App.kt
@@ -0,0 +1,34 @@
+/****************************************************************************************************
+ *                               Perhatian                                                          *
+ *                                                                                                  *
+ *          Agar dapat diperiksa dengan baik, hindari beberapa hal berikut:                         *
+ *                                                                                                  *
+ *          1. Mengubah kode yang berada di dalam fungsi main()                                     *
+ *          2. Mengubah atau menghapus kode yang sudah ada secara default                           *
+ *          3. Membuat fungsi baru yang bukan merupakan tugas latihan                               *
+ *          4. Mengubah struktur project (menghapus, mengedit, dan memindahkan package)             *
+ *                                                                                                  *
+ ***************************************************************************************************/
+
+package com.dicoding.exam.latihan_opsional5
+
+/**
+ *  TODO
+ *  Lengkapi fungsi di bawah dengan ketentuan sebagai berikut:
+ *  - Fungsi menerima input Number dengan panjang digit 2-10.
+ *  - Fungsi harus mengembalikan Integer yang merupakan hasil penjumlahan dari angka terkecil dan angka terbesar dari nilai yang diinput
+ *
+ *  Contoh:
+ *  Input = number=987234 -> Output = 11 (2+9)
+ *  Input = number=8812334 -> Output = 9 (1+8)
+ *  Input = number=10 -> Output = 1 (0+1)
+ */
+
+fun minAndMax(number: Number): Int {
+    return 0
+}
+
+fun main() {
+    println(minAndMax(987234) == 11)
+    println(minAndMax(8812334) == 9)
+}
