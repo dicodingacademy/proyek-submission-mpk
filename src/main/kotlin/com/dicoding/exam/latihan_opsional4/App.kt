ca258e6d8cc912e43e856ed4bb773a77ac172d3d
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-08-28 14:13:15 +0700

    feat: add `optional exam 4`

---
+++ src/main/kotlin/com/dicoding/exam/latihan_opsional4/App.kt
@@ -0,0 +1,36 @@
+/****************************************************************************************************
+ *                               Perhatian                                                          *
+ *                                                                                                  *
+ *          Agar dapat diperiksa dengan baik, hindari beberapa hal berikut:                         *
+ *                                                                                                  *
+ *          1. Mengubah kode yang berada di dalam fungsi main()                                     *
+ *          2. Mengubah atau menghapus kode yang sudah ada secara default                           *
+ *          3. Membuat fungsi baru yang bukan merupakan tugas latihan                               *
+ *          4. Mengubah struktur project (menghapus, mengedit, dan memindahkan package)             *
+ *                                                                                                  *
+ ***************************************************************************************************/
+
+package com.dicoding.exam.latihan_opsional4
+
+/**
+ *  TODO
+ *  Lengkapi fungsi di bawah dengan ketentuan sebagai berikut:
+ *  - Fungsi menerima input string dengan panjang karakter 1-100.
+ *  - Fungsi harus mengembalikan karakter yang ditengah dari string yang diinput.
+ *      - Jika panjang string bernilai genap, maka yang dikembalikan adalah 2 karakter.
+ *      - Jika panjang string bernilai ganjil, maka yang dikembalikan adalah 1 karakter.
+ *
+ *  Contoh:
+ *  Input = string=dicoding -> Output = od
+ *  Input = string=dicodingindonesia  -> Output = i
+ *  Input = string=A  -> Output = A
+ */
+
+fun getMiddleCharacters(string: String): String {
+    return ""
+}
+
+fun main() {
+    println(getMiddleCharacters("dicodingindonesia") == "i")
+    println(getMiddleCharacters("dicoding") == "od")
+}
