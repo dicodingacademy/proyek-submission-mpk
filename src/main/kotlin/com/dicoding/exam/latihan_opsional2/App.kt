24efd3032d3eceb05429a5ed88c7d45baaeafaa3
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-08-28 11:45:03 +0700

    feat: add `optional exam 2`

---
+++ src/main/kotlin/com/dicoding/exam/latihan_opsional2/App.kt
@@ -0,0 +1,20 @@
+/****************************************************************************************************
+ *                               Perhatian                                                          *
+ *                                                                                                  *
+ *          Agar dapat diperiksa dengan baik, hindari beberapa hal berikut:                         *
+ *                                                                                                  *
+ *          1. Mengubah nama method concatString()                                                  *
+ *          2. Mengubah struktur project (menghapus, mengedit, dan memindahkan package)             *
+ *                                                                                                  *
+ ***************************************************************************************************/
+
+package com.dicoding.exam.latihan_opsional2
+
+/**
+ *  TODO
+ *  Kerjakan latihan ini berdasarkan soal pada latihan_opsional2/Question.txt
+ */
+
+fun concatString(string1: String, string2: String): String {
+    return ""
+}
