f9acff8aeca0a6e0334b4e041aacc8222e240414
Ahmad Arif Faizin <faizin.arief@gmail.com>
2023-09-01 13:15:06 +0700

    Change wrong argument

--- src/main/kotlin/com/dicoding/exam/latihanopsional1/App.kt
+++ src/main/kotlin/com/dicoding/exam/latihanopsional1/App.kt
@@ -32,7 +32,7 @@ fun sumOfBigThree(vararg numbers: Int): Int {
 }
 
 fun main() {
-    println(sumOfBigThree(1, 2, 3, 4, 5, 6, 7, 8, 9, 104) == 27)
+    println(sumOfBigThree(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) == 27)
     println(sumOfBigThree(22, 20, 21, 25, 24) == 71)
     println(sumOfBigThree(100, 1000, 30, 1) == 1130)
 }
