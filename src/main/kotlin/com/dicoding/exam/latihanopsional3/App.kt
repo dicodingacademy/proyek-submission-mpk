d22913f8c7d07c7fd1ef84b19792a3865af7d6f0
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-09-15 09:44:58 +0700

    feat: add more sample for optional exam 3

--- src/main/kotlin/com/dicoding/exam/latihanopsional3/App.kt
+++ src/main/kotlin/com/dicoding/exam/latihanopsional3/App.kt
@@ -36,4 +36,5 @@ fun manipulateString(str: String, int: Int): String {
 fun main() {
     println(manipulateString("Dicoding000010", 100) == "Dicoding1000")
     println(manipulateString("dicoding", 99) == "dicoding99")
+    println(manipulateString("dicoding123", 10) == "dicoding1230")
 }
