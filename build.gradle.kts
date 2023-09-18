479da77aae4839aee4a19b69f770240676b09a53
Alfian Yusuf Abdullah <alfian@dicoding.com>
2023-09-18 22:51:46 +0700

    wrap `testReportExam` in `runBlocking` to adding some delay

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.jmailen.gradle.kotlinter.tasks.LintTask
 
 plugins {
@@ -29,9 +31,13 @@ dependencies {
 }
 
 tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-    finalizedBy("testReportExam")
+    runBlocking {
+        useJUnitPlatform()
+        delay(1000)
+        finalizedBy("testReportExam")
+    }
 }
+
 
 tasks.register<TestReportExam>("testReportExam")
 
