be0491a9c5716f905aa8c8645f0b9bc37c2dac11
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-09-22 13:42:51 +0700

    refactor: make `assertEqualsWith` receive `nullable`

--- src/test/kotlin/ExtentionsTest.kt
+++ src/test/kotlin/ExtentionsTest.kt
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Assertions
 
-fun assertEqualsWith(expected: Any, actual: Any, errMessage: String) {
+fun assertEqualsWith(expected: Any?, actual: Any?, errMessage: String) {
     if (expected != actual) {
         Assertions.fail<String>(errMessage)
     }
